532f7e7ac4eb200d23de0f1b23dc6918e6851c4f
officialkee <shikee.maloney@outlook.com>
2024-09-18 19:45:56 -0400

    added func to count line length

--- main.go
+++ main.go
@@ -1,5 +1,31 @@
 package main 
+import (
+	"bufio"
+	"os"
+	"fmt"
+	"reflect"
+)
 
 func main(){
-	
+	file, err := os.Open("test.txt")
+	if err != nil {
+		fmt.Println("Error opening file")
+		return
+	}
+	defer file.Close()
+
+	scanner := bufio.NewScanner(file)
+
+	for scanner.Scan(){
+		line := scanner.Text()
+
+		fmt.Println(reflect.TypeOf(line))
+		fmt.Println(line)
+		fmt.Println("Length of line is:",countLineLength(line))
+		
+	}
+}
+
+func countLineLength (line string) int {
+	return len(line)
 }
