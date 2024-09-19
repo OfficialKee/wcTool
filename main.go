5699bd6fc90e864b821612e89074bf971174ad70
officialkee <shikee.maloney@outlook.com>
2024-09-18 21:42:34 -0400

    added ability to take arguments, accurately counts words

--- main.go
+++ main.go
@@ -3,10 +3,12 @@ import (
 	"bufio"
 	"os"
 	"fmt"
+	"strings"
 
 )
 
 func main(){
+	 wordCount := 0 
 	args := os.Args
 	file, err := os.Open("test.txt")
 	if err != nil {
@@ -20,16 +22,25 @@ func main(){
 	for scanner.Scan(){
 		line := scanner.Text()
 		countLineLength(line)
+		 countWordLength(line,&wordCount)
 		for i, arg := range args {
 			fmt.Printf("Arg %d: %s\n", i, arg)
 		}
-
 		
 	}
+	fmt.Println("word count",wordCount)
 }
 
 func countLineLength (line string)  {
 	// fmt.Println(reflect.TypeOf(line))
 	fmt.Println(line)
 	fmt.Println("Length of line is:",len(line),"\n")
+}
+
+func countWordLength(line string, wc *int){
+	stringArr := strings.Split(line," ")
+	 *wc += len(stringArr)
+		// for _,word := range stringArr {
+		// 	fmtPrintln(word)
+		// }
 }
